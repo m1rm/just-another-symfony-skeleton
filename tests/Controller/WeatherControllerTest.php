d5092f77f2325159d630a88305a36d862ed2c7a1
Miriam <mail@miriam-mueller.com>
2025-10-26 11:01:49 +0100

    refactor

--- tests/Controller/WeatherControllerTest.php
+++ tests/Controller/WeatherControllerTest.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WeatherControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testWeatherDataShown(): void
     {
         // This calls KernelTestCase::bootKernel(), and creates a
         // "client" that is acting as the browser
@@ -17,5 +17,7 @@ class WeatherControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Welcome to weather!');
+        // in the happy-path, we expect the table to be shown
+        $this->assertSelectorTextContains('table', '1');
     }
 }
