88096251ecb689b68947a03f6303072b890285d2
Miriam <mail@miriam-mueller.com>
2025-11-01 12:08:28 +0100

    feat: use separate log for weather feature to showcase separate custom channels

--- tests/Controller/WeatherControllerTest.php
+++ tests/Controller/WeatherControllerTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
+use App\Service\WeatherService;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpClient\Exception\TransportException;
 
 class WeatherControllerTest extends WebTestCase
 {
@@ -19,5 +21,26 @@ class WeatherControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Welcome to weather!');
         // in the happy-path, we expect the table to be shown
         $this->assertSelectorTextContains('table', '1');
+    }
+
+    public function testWeatherErrorIsLogged(): void
+    {
+        $client = static::createClient();
+        $weatherServiceMock = $this->createMock(WeatherService::class);
+        $weatherServiceMock->method('getWeather')->willThrowException(new TransportException('test exception'));
+
+        $client->getContainer()->set(WeatherService::class, $weatherServiceMock);
+
+        $client->request('GET', '/weather');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('div.alert-danger', 'test exception');
+
+        $logFile = __DIR__ . '/../../var/log/weather.log';
+        $this->assertFileExists($logFile);
+        $this->assertStringContainsString('test exception', file_get_contents($logFile));
+
+        // Clean up the log file
+        unlink($logFile);
     }
 }
