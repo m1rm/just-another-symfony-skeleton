1e762b74f9fc46d3c20cee6e2c9bfe4b950b218c
Miriam <mail@miriam-mueller.com>
2025-11-01 12:04:58 +0100

    fix weather; Dto props named like keys in api response so serializer can auto-use them

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -5,9 +5,14 @@ namespace App\Controller;
 use App\Service\WeatherService;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 class WeatherController extends AbstractController
 {
     public function __construct(
@@ -21,7 +26,7 @@ class WeatherController extends AbstractController
     {
         try {
             $weather = $this->weatherService->getWeather();
-        } catch (\Exception $exception) {
+        } catch (TransportExceptionInterface | ClientExceptionInterface | RedirectionExceptionInterface | ServerExceptionInterface | HttpException $exception) {
             $this->logger->error($exception->getMessage());
             return $this->render('weather/index.html.twig', [
                 'weatherInfo' => null,
